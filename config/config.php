ce4ebc66cfe588b3ecb25571043aaebc270d361c
William Lety <william.lety@gmail.com>
2022-04-17 10:28:39 +0200

    Add DataFixturesCommand

--- config/config.php
+++ config/config.php
@@ -1,6 +1,7 @@
 <?php
 
 use Application\Console\DumpEnvCommand;
+use Application\Console\DataFixturesCommand;
 use Application\Console\GenerateAppKeyCommand;
 use Application\Console\DropDatabaseDoctrineCommand;
 use Application\Console\CreateDatabaseDoctrineCommand;
@@ -9,6 +10,7 @@ return [
     "console.commands" => \DI\add([
         'doctrine:database:create' => CreateDatabaseDoctrineCommand::class,
         'doctrine:database:drop' => DropDatabaseDoctrineCommand::class,
+        'fixtures:load' => DataFixturesCommand::class,
         'key:generate' => GenerateAppKeyCommand::class,
         'dump-env' => DumpEnvCommand::class,
     ])
